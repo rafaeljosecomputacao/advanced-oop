e76b68080d5dc2f62273621da17be406a9ea6f34
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2025-07-31 23:20:22 -0300

    Extension method

---
+++ Posts/Extensions/PostExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Posts.Extensions
+{
+    internal static class PostExtension
+    {
+        public static string ElapsedTime(this DateTime moment)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(moment);
+
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+
+        public static string Cut(this string sentence, int count)
+        {
+            if (sentence.Length <= count)
+            {
+                return sentence;
+            }
+            else
+            {
+                return sentence.Substring(0, count) + "...";
+            }
+        }
+    }
+}
---
+++ Posts/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Posts.Extensions;
+
+namespace Posts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime moment = new DateTime(2025, 07, 31, 10, 30, 20);
+            string sentence = "Good morning dear friends";
+            
+            Console.WriteLine(sentence.Cut(20) + " " + moment.ElapsedTime());
+        }
+    }
+}
